1248236d903a9ee4ff9f704200337db18fda2cf3
Yonghwi Kim <yh0921k@gmail.com>
2022-10-03 16:56:31 +0900

    test(Model, GameTest): 게임 클래스 테스트 구현
    GameTest.java
    - 하위 테스트 구현
      > isRunning(): 게임 시작 이후 게임 상태 조회
      > isOver(): 게임 종료 이후 게임 상태 조회
      > gameOver(): 사용자가 모든 숫자를 맞춘 경우 게임 종료 검증

---
+++ src/test/java/baseball/model/GameTest.java
@@ -0,0 +1,58 @@
+package baseball.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import baseball.constant.GameEnvironment;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    @DisplayName("게임이 시작된 이후 게임 상태 조회")
+    void isRunning() {
+        // then
+        assertThat(game.isRunning()).isTrue();
+        assertThat(game.isOver()).isFalse();
+    }
+
+    @Test
+    @DisplayName("게임이 종료된 이후 게임 상태 조회")
+    void isOver() {
+        // when
+        game.over();
+
+        assertThat(game.isRunning()).isFalse();
+        assertThat(game.isOver()).isTrue();
+    }
+
+    @Test
+    @DisplayName("사용자 시도가 3스트라이크인 경우 게임 종료")
+    void gameOver() {
+        // given
+        List<Integer> gameNumbers = new LinkedList<>(Arrays.asList(1, 2, 3));
+        List<Integer> userNumbers = new LinkedList<>(Arrays.asList(1, 2, 3));
+        GameCounter gameCounter = new GameCounter();
+
+        // when
+        gameCounter.calculate(userNumbers, gameNumbers);
+        if(gameCounter.getStrike() == GameEnvironment.NUMBER_SIZE) {
+            game.over();
+        }
+
+        // then
+        assertThat(game.isOver()).isTrue();
+        assertThat(game.isRunning()).isFalse();
+    }
+}
