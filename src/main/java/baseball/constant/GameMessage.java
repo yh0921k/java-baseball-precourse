0173e3ea8fada43798389f8aea0e30d92f482155
Yonghwi Kim <yh0921k@gmail.com>
2022-10-03 10:02:42 +0900

    feat(Constant, Message): 게임에 필요한 출력 메시지 클래스 구현
    GameMessage.java
    - 사용자 입력부 메시지, 게임 출력부 메시지 등 어플리케이션 전역에서 사용되는 메시지 클래스 구현

---
+++ src/main/java/baseball/constant/GameMessage.java
@@ -0,0 +1,13 @@
+package baseball.constant;
+
+public class GameMessage {
+    public static final String PLAYER_NUMBER_INPUT = "숫자를 입력해주세요 : ";
+    public static final String PLAYER_RESTART_INPUT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String GAME_STRIKE_OUTPUT = "%d스트라이크";
+    public static final String GAME_BALL_OUTPUT = "%d볼";
+    public static final String GAME_NOTHING_OUTPUT = "낫싱";
+
+    public static final String GAME_WIN_OUTPUT = "%d개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String GAME_ERROR_OUTPUT = "사용자 입력이 올바르지 않아 게임을 종료합니다.";
+}
