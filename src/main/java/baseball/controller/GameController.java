c66cd8c94bf12b7376cf0b5c5c777f677d13f20e
Yonghwi Kim <yh0921k@gmail.com>
2022-10-03 18:22:55 +0900

    feat(Controller, GameController): 게임 컨트롤러 클래스 구현
    GameController.java
    - View, Model 사이에서 사용자 입력과 데이터를 처리하는 역할을 하는 클래스
    - 사용자 입력에 따라 알맞은 비즈니스 로직을 처리하고 결과 View를 출력
    
    Method
    run()
    - main() 에서 불리는 핵심 컨트롤러 로직
    - 게임이 종료되지 않으면 계속 반복하여 내부 비즈니스 로직 및 UI 출력 로직을 담당
    
    startGame()
    - InputConsole의 사용자 입력 UI 출력 및 사용자 입력 처리 메서드를 호출
    
    guessNumber()
    - 게임 인스턴스에 대해 사용자 입력을 전달하고, 결과인 카운터 객체를 반환받음
    - 이후 OutputConsole을 이용하여 사용자 입력에 따라 계산된 결과 출력
    
    checkGameOver()
    - 사용자 승리로 게임이 종료되었다면 OutputConsole을 통해 해당 View 출력
    
    checkRestart()
    - 게임이 종료되었다면 사용자에게 재시작 여부를 입력받아 새로운 게임 시작

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,56 @@
+package baseball.controller;
+
+import baseball.constant.GameStatus;
+import baseball.model.Game;
+import baseball.model.GameCounter;
+import baseball.util.DataConverter;
+import baseball.view.InputConsole;
+import baseball.view.OutputConsole;
+
+public class GameController {
+
+    private static Game game;
+
+    public static void initGame() {
+        game = new Game();
+    }
+
+    public static void run() {
+        initGame();
+        do {
+            String playerNumber = startGame();
+            GameCounter counter = guessNumber(playerNumber);
+            boolean isGameOver = checkGameOver(counter);
+            checkRestart(isGameOver);
+        } while (game.isRunning());
+    }
+
+    private static String startGame() {
+        return InputConsole.readGameNumbers();
+    }
+
+    private static GameCounter guessNumber(String input) {
+        GameCounter counter = game.play(input);
+        OutputConsole.printNumberMatchesView(counter);
+        return counter;
+    }
+
+    private static boolean checkGameOver(GameCounter counter) {
+        if(game.isOver()) {
+            OutputConsole.printWinView(counter);
+            return true;
+        }
+        return false;
+    }
+
+    private static void checkRestart(boolean isGameOver) {
+        if(!isGameOver) {
+            return;
+        }
+
+        int restartNumber = DataConverter.stringToDigit(InputConsole.readRestartNumber());
+        if(restartNumber == GameStatus.START.getStatus()) {
+            game = new Game();
+        }
+    }
+}
