b6bf23b5723be1ea5e5a5f7e3594d9af60ea13eb
Yonghwi Kim <yh0921k@gmail.com>
2022-10-03 13:51:59 +0900

    test(Model, RandomNumberTest): 게임 번호 클래스 테스트 구현
    RandomNumberTest.java
    - 하위 테스트 구현
      > createRandomList(): 게임 조건에 맞는 사이즈의 난수 리스트 생성 검증
      > isNotContainsDuplicatedNumber()
        - 생성된 난수 리스트에 중복이 없는지 검증
        - 실제 외부 API에 해당하는 테스트로, 일종의 Mock 객체를 생성하여 상황을 제공
        - 외부 난수 생성 API가 중복 값을 생성할 경우 Set의 크기가 증가하지 않고,
        - GameEnvironment.NUMBER_SIZE와 같지 않아 프로덕션 코드의 조건을 통과하지 못함

---
+++ src/test/java/baseball/model/RandomNumberTest.java
@@ -0,0 +1,66 @@
+package baseball.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import baseball.constant.GameEnvironment;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+class RandomNumberTest {
+
+    private List<List<Integer>> randomNumbersList;
+    private Set<Integer> randomSet;
+
+    @BeforeAll
+    void setUp() {
+        randomNumbersList = new LinkedList<>();
+        randomNumbersList.add(Arrays.asList(1, 1, 2));
+        randomNumbersList.add(Arrays.asList(1, 2, 1));
+        randomNumbersList.add(Arrays.asList(2, 1, 1));
+        randomNumbersList.add(Arrays.asList(2, 2, 2));
+    }
+
+    @Test
+    @DisplayName("게임 제약조건에 맞는 사이즈의 값을 가진 난수 리스트 생성")
+    void createRandomList() {
+        // given
+        RandomNumber randomNumber = new RandomNumber();
+
+        // when
+        List<Integer> randomList = randomNumber.getRandomList();
+
+        // then
+        assertThat(randomList.size()).isEqualTo(GameEnvironment.NUMBER_SIZE);
+
+    }
+
+    @Test
+    @DisplayName("생성된 난수 리스트에 중복이 없는지 검증")
+    void isNotContainsDuplicatedNumber() {
+        for(List<Integer> randomList: randomNumbersList) {
+            // given
+            randomSet = new LinkedHashSet<>();
+
+            // when
+            applyDuplicatedValue(randomList);
+
+            // then
+            assertThat(randomSet.size()).isNotEqualTo(GameEnvironment.NUMBER_SIZE);
+        }
+    }
+
+    private void applyDuplicatedValue(List<Integer> duplicatedValueList) {
+        for (Integer integer : duplicatedValueList) {
+            randomSet.add(integer);
+        }
+    }
+}
