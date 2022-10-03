928951648938d18017e51dbde67c41e1cfce8397
Yonghwi Kim <yh0921k@gmail.com>
2022-10-03 12:59:02 +0900

    feat(Constant, Environment): 게임 번호 구성 필드 추가
    GameEnvironment.java
    - 게임 번호의 시작과 끝을 상수화

--- src/main/java/baseball/constant/GameEnvironment.java
+++ src/main/java/baseball/constant/GameEnvironment.java
@@ -2,4 +2,6 @@ package baseball.constant;
 
 public class GameEnvironment {
     public static final int NUMBER_SIZE = 3;
+    public static final int NUMBER_START = 0;
+    public static final int NUMBER_END = 9;
 }
