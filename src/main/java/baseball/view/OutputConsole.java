12e222d3296d433ad8db5655b200b9a91868e526
Yonghwi Kim <yh0921k@gmail.com>
2022-10-03 18:15:57 +0900

    feat(View, OutputConsole): 출력 UI 구현
    OutputConsole.java
    - 콘솔에서 사용자에게 보여질 출력을 담당할 클래스
    - 사용자 번호 추론 결과, 게임 승리, 게임 종료, 예외 상황 등의 출력을 담당
    
    Method
    printNumberMatchesView()
    - nothing(), ball(), strike() 메서드를 이용하여 사용자의 번호 추론 결과를 출력
    
    nothing(), ball(), strike()
    - GameCounter 인스턴스를 이용하여 사용자 입력 번호에 대한 게임 계산 결과를 반환
    
    printErrorView()
    - 예외 상황일 경우 출력
    
    printWinView()
    - win() 메서드를 사용하여 사용자가 이긴 경우에 따른 메시지를 출력
    
    win()
    - 카운터의 정보를 사용하여 승리 메시지를 생성 후 반환

---
+++ src/main/java/baseball/view/OutputConsole.java
@@ -0,0 +1,65 @@
+package baseball.view;
+
+import baseball.constant.GameEnvironment;
+import baseball.constant.GameMessage;
+import baseball.model.GameCounter;
+
+public class OutputConsole {
+
+    private static StringBuilder builder;
+
+    public static void printNumberMatchesView(GameCounter gameCounter) {
+        builder = new StringBuilder();
+        builder.append(nothing(gameCounter));
+        builder.append(ball(gameCounter));
+        builder.append(strike(gameCounter));
+
+        System.out.println(builder.toString());
+        builder.setLength(0);
+    }
+
+    private static String nothing(GameCounter gameCounter) {
+        if (gameCounter.isNothing()) {
+            return GameMessage.GAME_NOTHING_OUTPUT;
+        }
+        return "";
+    }
+
+    private static String ball(GameCounter gameCounter) {
+        if (gameCounter.hasBall()) {
+            return String.format(GameMessage.GAME_BALL_OUTPUT, gameCounter.getBall());
+        }
+        return "";
+    }
+
+    private static String strike(GameCounter gameCounter) {
+        StringBuilder builder = new StringBuilder();
+        if (gameCounter.hasBall()) {
+            builder.append(" ");
+        }
+
+        if (gameCounter.hasStrike()) {
+            return builder.append(String.format(GameMessage.GAME_STRIKE_OUTPUT, gameCounter.getStrike())).toString();
+        }
+        return "";
+    }
+
+    private static String win(GameCounter gameCounter) {
+        if (gameCounter.getStrike() == GameEnvironment.NUMBER_SIZE) {
+            return String.format(GameMessage.GAME_WIN_OUTPUT, gameCounter.getStrike());
+        }
+        return "";
+    }
+
+    public static void printErrorView() {
+        System.out.print(GameMessage.GAME_ERROR_OUTPUT);
+    }
+
+    public static void printWinView(GameCounter gameCounter) {
+        builder = new StringBuilder();
+        builder.append(win(gameCounter));
+
+        System.out.println(builder.toString());
+        builder.setLength(0);
+    }
+}
