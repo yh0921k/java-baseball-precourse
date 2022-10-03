106e68a42b2eef7096bfdff3600a2b2068d04c65
Yonghwi Kim <yh0921k@gmail.com>
2022-10-03 11:35:24 +0900

    feat(Constant, Status): 게임 상태 코드 리스트 반환 기능 구현
    GameStatus.java
    - 게임 상태 코드를 리스트로 반환하는 기능 구현

--- src/main/java/baseball/constant/GameStatus.java
+++ src/main/java/baseball/constant/GameStatus.java
@@ -1,5 +1,8 @@
 package baseball.constant;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public enum GameStatus {
     START(1), END(2);
 
@@ -11,5 +14,14 @@ public enum GameStatus {
 
     public int getStatus() {
         return status;
+    }
+
+    public static List<Integer> getStatusList() {
+        List<Integer> statusList = new LinkedList<>();
+        for (GameStatus gameStatus : GameStatus.values()) {
+            statusList.add(gameStatus.status);
+        }
+
+        return statusList;
     }
 }
