d3f6cc33834d336997e9ec06fc79a2ac0a1ade95
Yonghwi Kim <yh0921k@gmail.com>
2022-10-03 18:09:44 +0900

    feat(View, InputConsole): 입력 UI 구현
    InputConsole.java
    - 콘솔에서 사용자 입력을 담당할 UI 클래스
    - 사용자의 게임 번호 입력, 사용자의 재시작 번호 입력을 담당
    - 추가적으로 GameValidator를 이용해 사용자 입력이 유효하지 않은 경우 예외 처리
    
    Method
    readGameNumbers(): 사용자의 게임 번호 입력 기능
    readRestartNumber(): 사용자의 재시작 번호 입력 기능
    occursErrorBy(): 유효성 검증 결과에 따라 예외를 발생
    
    printNumberInputView(), printRestartInputView()
    - 상황에 따라 GameMessage 클래스의 메시지를 취하여 사용자 입력을 유도

---
+++ src/main/java/baseball/view/InputConsole.java
@@ -0,0 +1,40 @@
+package baseball.view;
+
+import baseball.constant.GameMessage;
+import baseball.validation.GameValidator;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputConsole {
+    public static String readGameNumbers() {
+        String input = printNumberInputView();
+        boolean isValid = GameValidator.isValidGameNumbers(input);
+        occursErrorBy(isValid);
+
+        return input;
+    }
+
+    public static String readRestartNumber() {
+        String input = printRestartInputView();
+        boolean isValid = GameValidator.isValidRestartNumber(input);
+        occursErrorBy(isValid);
+
+        return input;
+    }
+
+    private static void occursErrorBy(boolean isValid) {
+        if(!isValid) {
+            OutputConsole.printErrorView();
+            throw new IllegalArgumentException(GameMessage.GAME_ERROR_OUTPUT);
+        }
+    }
+
+    private static String printNumberInputView() {
+        System.out.print(GameMessage.PLAYER_NUMBER_INPUT);
+        return Console.readLine();
+    }
+
+    private static String printRestartInputView() {
+        System.out.println(GameMessage.PLAYER_RESTART_INPUT);
+        return Console.readLine();
+    }
+}
